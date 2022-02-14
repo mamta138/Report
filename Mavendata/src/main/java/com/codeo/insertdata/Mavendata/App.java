545e0fc3e792c94fa51a3015281e2d5b4ebd804b
sudarshan <career@codeographsolutions.com>
2022-02-14 18:57:58 +0530

    changes made by subho

--- Mavendata/src/main/java/com/codeo/insertdata/Mavendata/App.java
+++ Mavendata/src/main/java/com/codeo/insertdata/Mavendata/App.java
@@ -23,7 +23,8 @@ public class App
    	 Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/student_info","root","");
           // statement 
    	 Statement st= con.createStatement();
-   	 String query  = "INSERT INTO table_i (student_name, email_id, mobileno, addreass, rollno) values ('Mahi','mahi46@gmail.com','mahal','7878712539','06')";
+   	 String query=null;
+   query  = "INSERT INTO table_i (student_name, email_id, mobileno, addreass, rollno) values ('Mahi','mahi46@gmail.com','mahal','7878712539','06')";
    	 //execute the query
    	st.executeUpdate(query);
    	 st.close();
